b569169a60225489abe3dc86d283b5f655f841c3
bangbang93 <bangbang93@163.com>
2019-03-08 11:01:25 +0800

    support connect

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as path from 'path'
 import {server} from './server'
 
 server.listen(8080, '0.0.0.0')
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import {Request, Response} from 'express'
-import {ClientRequestArgs, IncomingMessage, ServerResponse} from 'http'
-import * as http from 'http'
-import * as path from 'path'
-import * as fs from 'fs-extra'
-import * as url from 'url'
-import * as express from 'express'
 import * as colors from 'colors'
+import * as express from 'express'
+import {Request, Response} from 'express'
+import * as fs from 'fs-extra'
+import * as http from 'http'
+import {ClientRequestArgs, IncomingMessage} from 'http'
+import * as net from 'net'
+import {Socket} from 'net'
+import * as path from 'path'
+import * as url from 'url'
 
 const urlReg = /\.(pkg|pup)$/
 const dataDir = path.join(__dirname, '../data')
-const app = express()
+export const app = express()
 
 export const server = http.createServer(app)
 
-app.use(onRequest)
+app.all('*', onRequest)
+server.on('connect', onConnect)
 
 async function onRequest(req: Request, res: Response) {
   const filename = path.basename(req.url)
@@ -39,4 +42,31 @@ async function onRequest(req: Request, res: Response) {
   })
 
   req.pipe(proxyReq)
+}
+
+function onConnect(req: IncomingMessage, socket: Socket) {
+  console.log(colors.gray(`connect ${req.url}`))
+
+  const res = new http.ServerResponse(req)
+  res.shouldKeepAlive = false
+  res.chunkedEncoding = false
+  res.assignSocket(socket)
+  res.once('finish', () => {
+    res.detachSocket(socket)
+    socket.end()
+  })
+
+  const [host, port] = req.url.split(':')
+
+  const target = net.connect(parseInt(port), host)
+  target.on('connect', () => {
+    res.writeHead(200)
+    res['_send']('')
+    socket.pipe(target)
+    target.pipe(socket)
+  })
+  target.on('close', () => {
+    socket.destroy()
+    target.destroy()
+  })
 }
